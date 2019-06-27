889461d497c304bf4c41c27d02f71210e879ed77
Chernyshov <xpom55@mail.ru>
2019-06-28 00:36:07 +0300

    ~ added timer on every 10 seconds

--- main.go
+++ main.go
@@ -1,28 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/distatus/battery"
 	"github.com/getlantern/systray"
+	"github.com/robfig/cron"
 )
-
-func main() {
-	systray.Run(onReady, onExit)
-}
-
-func onReady() {
-	systray.SetTitle("Hello Natasha - Valerka")
-	getPrice()
-}
 
 // h2 text-semi-bold details-panel-item--price__value
 // https://coinmarketcap.com/currencies/bitcoin/#markets
-func onExit() {
 
+type state struct {
+	Price string
+	Cron  *cron.Cron
 }
 
-func getPrice() {
+func main() {
+	s := &state{}
+	systray.Run(s.onReady, s.onExit)
+}
+
+func (s *state) onReady() {
+	// systray.SetTitle("Hello Natasha - Valerka")
+	s.updatePrice()
+	s.Cron = cron.New()
+	s.Cron.AddFunc("@every 10s", s.updatePrice)
+	s.Cron.Start()
+	// getBattery()
+}
+
+func (s *state) onExit() {
+	s.Cron.Stop()
+}
+
+func getBattery() {
+	batteries, err := battery.GetAll()
+	if err != nil {
+		fmt.Println("Could not get battery info!")
+		return
+	}
+	for i, battery := range batteries {
+		fmt.Printf("Bat%d: ", i)
+		fmt.Printf("state: %f, ", battery.State)
+		fmt.Printf("current capacity: %f mWh, ", battery.Current)
+		fmt.Printf("last full capacity: %f mWh, ", battery.Full)
+		fmt.Printf("design capacity: %f mWh, ", battery.Design)
+		fmt.Printf("charge rate: %f mW, ", battery.ChargeRate)
+		fmt.Printf("voltage: %f V, ", battery.Voltage)
+		fmt.Printf("design voltage: %f V\n", battery.DesignVoltage)
+
+		// systray.SetTitle("B - ", battery.State)
+	}
+}
+
+func (s *state) updatePrice() {
 	url := "https://coinmarketcap.com/currencies/bitcoin/#markets"
 	res, err := http.Get(url)
 	if err != nil {
@@ -40,6 +74,6 @@ func getPrice() {
 	}
 
 	price := doc.Find(".details-panel-item--price__value").Text()
-
+	fmt.Printf(price)
 	systray.SetTitle("BTC $" + price)
 }
