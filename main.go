5a25e93d9866a7b9849c2a2c262af5770330e415
Chernyshov <xpom55@mail.ru>
2019-06-28 00:04:14 +0300

    ~ get url BTC price

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 	"github.com/getlantern/systray"
 )
 
@@ -10,7 +13,33 @@ func main() {
 
 func onReady() {
 	systray.SetTitle("Hello Natasha - Valerka")
+	getPrice()
 }
+
+// h2 text-semi-bold details-panel-item--price__value
+// https://coinmarketcap.com/currencies/bitcoin/#markets
 func onExit() {
 
+}
+
+func getPrice() {
+	url := "https://coinmarketcap.com/currencies/bitcoin/#markets"
+	res, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return
+	}
+
+	price := doc.Find(".details-panel-item--price__value").Text()
+
+	systray.SetTitle("BTC $" + price)
 }
